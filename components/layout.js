3feb6564d285ba5f7449aa94ed4c31ce00d8cbda
Alexandre <alex.teixeira2201@gmail.com>
2022-12-22 15:09:00 +0000

    changed name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Alexandre Teixeira';
+const name = 'Regina Silva';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
